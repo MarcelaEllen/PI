8b920f17c72dba071e109593daa9e5df49f72ded
Kelveny96 <143358497+Kelveny96@users.noreply.github.com>
2023-09-28 16:45:04 -0300

    Adição da página de cadastro do app da sincopeças

---
+++ assets/questions.js
@@ -0,0 +1,94 @@
+export default [
+    {
+      question: "Em um programa de televisão sobre mercado de trabalho, três especialista em atendimento ao cliente tecem os seguintes comentários sobre os princípios da venda pessoal: <br> <br> <b> Alice </b> - A época do vendedor esperto já passou, e no seu lugar está o profissional que sabe levantar boas perguntas, ouvir e aprender. <br> <b>Antônio</b> - Os vendedores eficazes possuem um instinto natural que se revela especialmente no momento de convencer as pessoas. <br> <b>Pedro</b> - A função do vendedor é aumentar o número de clientes de sua empresa, focando seus esforços na realização de negócios imediatos. É(São) correta(s) a(s) afirmação(ões) de",
+      answers: [
+        { option: "Alice, Antônio e Pedro.", correct: true },
+        { option: "Alice e Antônio, apenas.", correct: false },
+        { option: "Pedro e Antônio, apenas.", correct: false },
+        { option: "Pedro, apenas.", correct: false },
+        { option: "Alice, apenas.", correct: false },
+      ],
+    },
+    {
+      question: "O atendimento ao cliente pode ser feito de forma presencial ou por meio de telefone e requer conhecimentos, atitudes e comportamentos adequados. Em relação às atitudes e comportamentos, assinale a alternativa que apresenta alguns dos aspectos implícitos no atendimento ao público.<br><br>",
+      answers: [
+        { option: "Palavras, diferenciação e apoio pessoal.", correct: false },
+        { option: "Aparência, expressão corporal e voz.", correct: true },
+        { option: "Roteiro padrão, idoneidade e sigilo.", correct: false },
+        { option: "Honestidade, praticidade e visão.", correct: false },
+        { option: "Confrontação, impessoalidade e zelo", correct: false },
+      ],
+    },
+    {
+      question: "Atendimento corresponde ao ato de atender, ou seja, ao ato de prestar atenção às pessoas com as quais mantemos contato. O “bom atendimento é aquele que procura verdadeiramente atender às expectativas do público. Sobre o assunto, analise as afirmativas abaixo: <br><br>I. A qualidade do atendimento prestado depende da capacidade de se comunicar com o público e da mensagem transmitida. <br> II. Inicie a conversa com uma atitude amigável e prestativa. Seja proativo, pergunte à pessoa: ''Em que posso ajudar?''. <br> III. Mantenha a calma mesmo com pessoas menos educadas. Jamais as interrompa, discuta ou seja agressivo com elas.<br><br> O número de afirmativas INCORRETAS corresponde a:",
+      answers: [
+        { option: "Zero.", correct: false },
+        { option: "Uma.", correct: false },
+        { option: "Duas.", correct: true },
+        { option: "Três.", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 8 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+        { option: "4", correct: false },
+      ],
+    },
+  ];
